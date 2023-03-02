c596de5446809de1df3638f57b7ab32c50e24dc4
Benjamin Campomenosi <benjamin.campomenosi@sonarsource.com>
2023-03-02 10:53:14 +0100

    SONAR-18408 - Add CirrusCI config for build

--- src/main/java/org/sonar/scm/git/blame/BlameThreadFactory.java
+++ src/main/java/org/sonar/scm/git/blame/BlameThreadFactory.java
@@ -21,14 +21,14 @@ package org.sonar.scm.git.blame;
 
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
-import org.jetbrains.annotations.NotNull;
+import javax.annotation.Nonnull;
 
 public class BlameThreadFactory implements ThreadFactory {
   private static final String NAME_PREFIX = "git-blame-";
   private final AtomicInteger count = new AtomicInteger(0);
 
   @Override
-  public Thread newThread(@NotNull Runnable r) {
+  public Thread newThread(@Nonnull  Runnable r) {
     Thread t = new Thread(r);
     t.setName(NAME_PREFIX + count.getAndIncrement());
     t.setDaemon(true);
