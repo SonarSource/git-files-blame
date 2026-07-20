d145db5bde881cf93f59cfd11ba51061ddb26d7d
Julien HENRY <julien.henry@sonarsource.com>
2026-07-20 10:34:24 +0200

    Apply suggestions from code review
    Co-authored-by: Alban Auzeill <alban.auzeill@sonarsource.com>

--- src/test/java/org/sonar/scm/git/blame/RepositoryBlameCommandIT.java
+++ src/test/java/org/sonar/scm/git/blame/RepositoryBlameCommandIT.java
@@ -499,7 +499,7 @@ class RepositoryBlameCommandIT extends AbstractGitIT {
   void blame_whenContentGiven_thenLinesHaveNullBlame() throws IOException, GitAPIException {
     createFile(baseDir, "fileA", "line1");
     String c1 = commit("fileA");
-    String unsavedContent = String.join("\n", "line1", "newLine") + "\n";
+    String unsavedContent = "line1\nnewLine\n";
     UnaryOperator<String> fileAContentProvider = filePath -> "fileA".equals(filePath) ? unsavedContent : null;
 
     BlameResult result = blame
